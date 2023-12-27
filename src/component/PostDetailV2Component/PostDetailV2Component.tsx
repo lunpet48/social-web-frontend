c004fe0c371ec3c590644f4125db7149365efecd
lunpet48 <phaboss231@gmail.com>
2023-12-27 20:42:12 +0700

    [fix-bug] sửa xong bug 18

--- src/component/PostDetailV2Component/PostDetailV2Component.tsx
+++ src/component/PostDetailV2Component/PostDetailV2Component.tsx
@@ -281,7 +281,7 @@ const PostDetailV2Component = ({ postId }: { postId: string }) => {
         const data = await response.json();
         setLikeList(data.data);
       }
-    } catch (e) {}
+    } catch (e) { }
   };
   const handleOpenLikeList = () => {
     fetchLikesOfPost();
@@ -309,19 +309,19 @@ const PostDetailV2Component = ({ postId }: { postId: string }) => {
           <div className="flex grid grid-cols-1">
             <div className="header border-b pt-4 pb-4 pl-2 pr-2 flex justify-between items-center">
               <div className="left flex flex-row items-center">
-                <div className="user-img h-10 w-10 border rounded-full overflow-hidden mr-2">
+                <a className="user-img h-10 w-10 border rounded-full overflow-hidden mr-4" href={`/profile/${user?.username}`}>
                   <img
                     alt="avatar"
                     className="_6q-tv"
-                    data-testid="user-avatar"
                     draggable="false"
                     src={user?.avatar}
                   />
-                </div>
-                <div className="user-name-and-place flex flex-col">
+                </a>
+                <a className="user-name-and-place flex flex-col no-underline text-gray-900 hover:text-gray-400" href={`/profile/${user?.username}`}>
                   <span className="text-sm font-bold">{user?.username}</span>
-                  <span className="text-xs font-light text-gray-900"></span>
-                </div>
+                  <span className="text-xs font-light text-gray-900">
+                  </span>
+                </a>
                 {post.createdAt !== post.updatedAt ? (
                   <>
                     <svg
@@ -387,18 +387,19 @@ const PostDetailV2Component = ({ postId }: { postId: string }) => {
                     <>
                       <div className="content pl-2 pr-2 pt-4 pb-2 flex">
                         <div className="left flex flex-row">
-                          <div className="user-img h-10 w-10 border rounded-full overflow-hidden mr-2">
+                          <a className="user-img h-10 w-10 border rounded-full overflow-hidden mr-4" href={`/profile/${user?.username}`}>
                             <img
                               alt="avatar"
                               className="_6q-tv"
-                              data-testid="user-avatar"
                               draggable="false"
                               src={user?.avatar}
                             />
-                          </div>
+                          </a>
                         </div>
                         <div className="user-name-and-place mt-2">
-                          <span className="text-sm font-bold">{user?.username} </span>
+                          <a className="user-name-and-place no-underline text-gray-900 hover:text-gray-400 mr-1" href={`/profile/${user?.username}`}>
+                            <span className="text-sm font-bold">{user?.username}</span>
+                          </a>
                           <span className="text-sm font-light text-gray-900">{post.caption}</span>
                         </div>
                       </div>
--- src/component/comment.tsx
+++ src/component/comment.tsx
@@ -53,18 +53,19 @@ const CommentComponent = ({ comment }: any) => {
         <>
             <div className='content pl-2 pr-2 pt-4 pb-2 flex'>
                 <div className="left flex flex-row">
-                    <div className="user-img h-10 w-10 border rounded-full overflow-hidden mr-2">
+                    <a className="user-img h-10 w-10 border rounded-full overflow-hidden mr-4" href={`/profile/${user?.username}`}>
                         <img
                             alt="avatar"
                             className="_6q-tv"
-                            data-testid="user-avatar"
                             draggable="false"
                             src={user?.avatar}
                         />
-                    </div>
+                    </a>
                 </div>
-                <div className="user-name-and-place mt-2">
-                    <span className="text-sm font-bold">{user?.username} </span>
+                <div className="user-name-and-place mt-2 flex justify-stretch">
+                    <a className="user-name-and-place no-underline text-gray-900 hover:text-gray-400 mr-1" href={`/profile/${user?.username}`}>
+                        <span className="text-sm font-bold">{user?.username}</span>
+                    </a>
                     <span className="text-sm font-light text-gray-900">
                         {comment.comment}
                     </span>
