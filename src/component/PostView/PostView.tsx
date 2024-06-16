0fc9f1d5f64cde120830ddc38049c3639d05e737
khoihuynh300 <khoihuynh300@gmail.com>
2024-06-16 23:12:30 +0700

    stop loading when posts is out

--- src/component/PostView/PostView.tsx
+++ src/component/PostView/PostView.tsx
@@ -9,6 +9,7 @@ import useElementOnScreen from '@/hooks/useElementOnScreen';
 const PostView = () => {
   const [posts, setPosts] = useState<post[]>([]);
   const [loadingPage, setLoadingPage] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
 
   const [paging, setPaging] = useState<paging>({ pageNo: 0, pageSize: 3 });
 
@@ -17,7 +18,7 @@ const PostView = () => {
   const fetchPosts = async (paging: paging) => {
     const posts: post[] = await getPosts(paging);
     if (posts.length === 0) {
-      /// insert code here: remove loading at bottom and show message that there are no post to show
+      setIsEnd(true);
     } else {
       setPosts((prev) => [...prev, ...posts]);
     }
@@ -46,13 +47,15 @@ const PostView = () => {
       {posts.map((post) => (
         <Post key={post.postId} post={post} />
       ))}
-      <div style={{ position: 'relative' }}>
-        <Loading height='50px' />
-        <div
-          ref={ref as React.RefObject<HTMLDivElement>}
-          style={{ position: 'absolute', top: '-500px' }}
-        />
-      </div>
+      {!isEnd && (
+        <div style={{ position: 'relative' }}>
+          <Loading height='50px' />
+          <div
+            ref={ref as React.RefObject<HTMLDivElement>}
+            style={{ position: 'absolute', top: '-500px' }}
+          />
+        </div>
+      )}
     </div>
   );
 };
